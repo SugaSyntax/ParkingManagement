e957556767767269599f98f959ed4d710a72b093
SugaSyntax <155683364+SugaSyntax@users.noreply.github.com>
2024-04-21 23:01:42 +0100

    commit from vscode

--- ParkingManagement.API/obj/Debug/net8.0/ParkingManagement.API.AssemblyInfo.cs
+++ ParkingManagement.API/obj/Debug/net8.0/ParkingManagement.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ParkingManagement.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27f1d2461827171f64d87dbdcf1f3751f4ceaf2e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79868cc5a4c38f5014adfa861827772142a9ddaf")]
 [assembly: System.Reflection.AssemblyProductAttribute("ParkingManagement.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ParkingManagement.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
